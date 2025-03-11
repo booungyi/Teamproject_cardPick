6b4673bbdb64ebba30a93f034ac82ed796620918
booungyi <ansdlsgur0123@gmail.com>
2025-03-11 13:47:50 +0900

    commit initialize

---
+++ src/main/java/team/cardpick_project/cardpick/CardpickApplication.java
@@ -0,0 +1,13 @@
+package team.cardpick_project.cardpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardpickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/cardpick_project/cardpick/CardpickApplicationTests.java
@@ -0,0 +1,13 @@
+package team.cardpick_project.cardpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardpickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
