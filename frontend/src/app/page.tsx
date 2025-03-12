5d7a3a8b115949d929335393effb6a38738eec67
jun <mmy519@naver.com>
2025-03-12 14:15:40 +0900

    fix : 맞춤 카드 검색 버튼 추가

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,22 +1,33 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import styles from "../styles/Home.module.css"; // ✅ 상대 경로로 수정
+
+import styles from "../styles/Home.module.css";
+import {useRouter} from "next/navigation"; // ✅ 상대 경로로 수정
 
 export default function Home() {
     const router = useRouter();
 
     return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>💳 나에게 딱 맞는 카드를 찾아보세요!</h1>
-            <p className={styles.description}>간단한 테스트를 통해 맞춤 카드를 추천해드립니다.</p>
+        <>
+            <div className={styles.container}>
+                <h1 className={styles.title}>💳 나에게 딱 맞는 카드를 찾아보세요!</h1>
+                <p className={styles.description}>간단한 테스트를 통해 맞춤 카드를 추천해드립니다.</p>
 
-            <button
-                onClick={() => router.push("/quiz")}
-                className={styles.button}
-            >
-                테스트 시작하기 🚀
-            </button>
-        </div>
+                <button
+                    onClick={() => router.push("/quiz")}
+                    className={styles.button}
+                >
+                    테스트 시작하기 🚀
+                </button>
+
+                <p className={styles.description}>맞춤 혜택으로 직접 찾기</p>
+
+                <button onClick={() => router.push("/selectedBenefit")}
+                        className={styles.button}
+                >
+                    맞춤 카드 검색 🔍
+                </button>
+            </div>
+        </>
     );
 }
