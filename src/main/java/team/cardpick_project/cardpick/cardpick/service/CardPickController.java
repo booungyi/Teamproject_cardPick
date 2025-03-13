59f6920cfed79b0a753cd3601c3344807627fbb3
HaeJoon <wjdgowns4414@gmail.com>
2025-03-13 16:14:55 +0900

    getCountByConditions 메서드 requestParam 인자 필수 여부 수정

--- src/main/java/team/cardpick_project/cardpick/cardpick/service/CardPickController.java
+++ src/main/java/team/cardpick_project/cardpick/cardpick/service/CardPickController.java
@@ -16,12 +16,12 @@ public class CardPickController {
 
     //선택된 조건에 맞는 카드 추천
     @GetMapping("/conditions")
-    public List<CardResponse> getCardsByConditions(@RequestParam(required = false) String issuer, @RequestParam(required = false) List<String> categories){
+    public List<CardResponse> getCardsByConditions(@RequestParam(required = false) String issuer, @RequestParam List<String> categories){
         return cardPickService.getCardsByConditions(issuer, categories);
     }
     //현재 선택된 조건에 맞는 카드 개수
     @GetMapping("/conditions/count")
-    public Long getCountByConditions(@RequestParam(required = false) String issuer, @RequestParam List<String> categories){
+    public Long getCountByConditions(@RequestParam(required = false) String issuer, @RequestParam(required = false) List<String> categories){
         return cardPickService.getCountByConditions(issuer, categories);
     }
     //성향에 맞는 카드 추천
