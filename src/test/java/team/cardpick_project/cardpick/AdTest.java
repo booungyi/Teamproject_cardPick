aa5b0971ac5593a409d25ff37a4219db563dbb3e
jun <mmy519@naver.com>
2025-03-28 17:49:58 +0900

    fix: 광고 생성, 조회, 수정 테스트 1차 완료

--- src/test/java/team/cardpick_project/cardpick/AdTest.java
+++ src/test/java/team/cardpick_project/cardpick/AdTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import team.cardpick_project.cardpick.cardpick.cardpickDto.AdResponse;
 import team.cardpick_project.cardpick.cardpick.cardpickDto.CreateAdRequest;
+import team.cardpick_project.cardpick.cardpick.cardpickDto.CreateAdTermRequest;
 
 import java.time.LocalDateTime;
 
@@ -28,6 +29,49 @@ public class AdTest {
                 .statusCode(200)
                 .extract()
                 .as(AdResponse.class);
+
+        assertThat(광고).isNotNull();
+    }
+
+    @Test
+    void 광고_조회() {
+        //조회
+        RestAssured
+                .given()
+                .when()
+                .get("/ad") // 서버로 GET /products 요청
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void 광고_기간수정() {
+        AdResponse 광고 = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(new CreateAdRequest(
+                        1L,
+                        LocalDateTime.parse("2025-03-26T00:00:00"),
+                        LocalDateTime.parse("2025-04-25T00:00:00")))
+                .when()
+                .post("/ad") // POST /members 요청
+                .then().log().all()
+                .statusCode(200)
+                .extract()
+                .as(AdResponse.class);
+
+        //수정
+        RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .pathParam("adCardId",1L)
+                .body(new CreateAdTermRequest(
+                        LocalDateTime.parse("2025-03-26T00:00:00"),
+                        LocalDateTime.parse("2025-04-20T00:00:00")
+                ))
+                .when()
+                .put("/ad/{adCardId}")
+                .then().log().all()
+                .statusCode(200);
 
         assertThat(광고).isNotNull();
     }
